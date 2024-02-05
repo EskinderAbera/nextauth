50479ba7f2d892879d7be568164c70a36b4a25eb
EskinderAbera <eske17.ea@gmail.com>
2024-02-05 15:45:18 +0300

    implement server action for register

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,39 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
+
 import { RegisterSchema } from "@/schema";
+import { db } from "@/lib/db";
+import { error } from "console";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFiels = RegisterSchema.safeParse(values);
   if (!validatedFiels.success) {
     return { error: "Invalid Fields" };
   }
+
+  const { email, password, name } = validatedFiels.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    return { error: "Email is already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: send verification token email
+
   return {
     success: "Email sent!",
   };
